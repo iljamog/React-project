96c38cce6bba72eddb7ab64d23ebe72dd072e758
iljamog <iljamog@tlu.ee>
2021-11-19 14:03:54 +0200

    MongoDB setup #2

--- backend-node/server/index.js
+++ backend-node/server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+mongoose.connect("mongodb+srv://root:root@big-burps-pennywhistle.pknyp.mongodb.net/users?retryWrites=true&w=majority",
+{
+  useNewUrlParser: true,
+}
+);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
