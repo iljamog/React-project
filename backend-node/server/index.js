609deb388374a65ac0318541fd2bda3fecacf7b7
iljamog <iljamog@tlu.ee>
2021-11-25 06:39:49 +0200

    Login/Register API + functionality reupload

--- backend-node/server/index.js
+++ backend-node/server/index.js
@@ -2,17 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const dotenv = require("dotenv");
+const authRoutes = require('../routes/auth');
+
+dotenv.config();
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-mongoose.connect("mongodb+srv://root:root@big-burps-pennywhistle.pknyp.mongodb.net/users?retryWrites=true&w=majority",
+mongoose.connect(process.env.MDB_CONNECT,
 {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+},
+(err) => {
+  if (err) return console.error(err);
+  console.log("DB connected");
 }
 );
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
