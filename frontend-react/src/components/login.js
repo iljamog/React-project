6b7365a5cbc51c2a53d9632c6d918ab83f7f1994
kertliinat <43129546+kertliinat@users.noreply.github.com>
2021-12-05 22:57:12 +0200

    Added styling for register and login
    Also reworked the need for antd in those two files

--- frontend-react/src/components/login.js
+++ frontend-react/src/components/login.js
@@ -1,61 +1,59 @@
-import { Form, Input, Button } from 'antd';
-import { useState} from "react"
+import './login.css';
+import { useState } from "react";
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState('')
 
-    const LogIn = async (e) => {
-        setEmail(e.email)
-        setPassword(e.password)
-
-        const userData = {
-            email: e.email,
-            password: e.password
-          }
-      
-          const res = await fetch('http://localhost:3000/api/auth/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData),
-          })
-          
-          const returnData = await res.json()
-      
-          if(returnData.token) {
-            console.log("Successfully logged in")
-          } else {
-            let errors = ''
-              if (returnData.error) {
-                errors = returnData.error
-              } else {
-                for (let i = 0; i < returnData.msg.length; i++) {
-                  errors += returnData.msg[i].param[0].toUpperCase() + returnData.msg[i].param.slice(1) + ' ' + returnData.msg[i].msg + '\n'
-                }
-              }
-              setError(errors)
-          }    
+  function handleEmailChange(e){
+    setEmail(e.target.value);
+  }
+  function handlePasswordChange(e){
+    setPassword(e.target.value);
+  }
+  const LogIn = async (e) => {
+    const userData = {
+      email: email,
+      password: password
     }
+    const res = await fetch('http://localhost:3000/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData),
+    })
 
-    return (
-        <Form
-            labelCol={{ span: 7 }}
-            wrapperCol={{ span: 6 }}
-            initialValues={{ remember: true }}
-            autoComplete="off"
-            onFinish={LogIn}
-        >
-            <Form.Item label="E-mail" name="email" required><Input/></Form.Item>
+    const returnData = await res.json()
 
-            <Form.Item label="Password" name="password" required><Input.Password/></Form.Item>
+    if (returnData.token) {
+      console.log("Successfully logged in")
+    } else {
+      let errors = ''
+      if (returnData.error) {
+        errors = returnData.error
+      } else {
+        for (let i = 0; i < returnData.msg.length; i++) {
+          errors += returnData.msg[i].param[0].toUpperCase() + returnData.msg[i].param.slice(1) + ' ' + returnData.msg[i].msg + '\n'
+        }
+      }
+      setError(errors);
+      console.log(error);
+    }
+  }
 
-            <Form.Item wrapperCol={{ offset: 9, span: 5 }}>
-                <Button type="primary" htmlType="submit">Submit</Button>
-            </Form.Item>
-        </Form>
-    )
+  return (
+    <div class="form-body">
+      <div class="mainform">
+        <div class="loginform">
+          <label class="loginlabel">Login</label>
+          <input class="input" label="E-mail" placeholder="E-mail" onChange={handleEmailChange} type="email" required />
+          <input class="input" label="Password" placeholder="Password" onChange={handlePasswordChange} type="password" required />
+          <button class="button" type="primary" onClick={LogIn}>Submit</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
--- frontend-react/src/components/register.js
+++ frontend-react/src/components/register.js
@@ -1,24 +1,46 @@
-import {Form, Input, Button, Typography} from 'antd';
+import './register.css'
 import {useState} from "react"
 
 function Register() {
     const [error, setError] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+    const [firstname, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
+
+
+    function handleEmailChange(e){
+        setEmail(e.target.value);
+    }
+    function handlePasswordChange(e){
+        setPassword(e.target.value);
+    }
+    function handleRepeatPasswordChange(e){
+        setRepeatPassword(e.target.value);
+    }
+    function handleFirstNameChange(e){
+        setFirstName(e.target.value);
+    }
+    function handleLastNameChange(e){
+        setLastName(e.target.value);
+    }
 
     const createUser = async (user) => {
         const data = {
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email,
-            password: user.password
+            firstName: firstname,
+            lastName: lastname,
+            email: email,
+            password: password
         }
 
-        if (!user.firstName || !user.lastName) {
-            setError('Please enter your full name');
-        } else if (!user.email) {
+        if (!firstname || !lastname) {
             setError('Please enter your email');
-        } else if (!user.password) {
+        } else if (!email) {
+            setError('Please enter your email');
+        } else if (!password) {
             setError('Please enter your password');
-        } else if (!user.password !== user.repeatPassword) {
+        } else if (!password !== repeatPassword) {
             setError('The passwords dont match, please try again');
         } else {
             const response = await fetch('http://localhost:3000/api/auth/signup', {
@@ -44,29 +66,24 @@ function Register() {
                 }
                 setError(errors)
             }
+        }if(error){
+            console.log(error);
         }
     }
-    const { Text } = Typography;
     return (
-        <Form
-            labelCol={{span: 7}}
-            wrapperCol={{span: 6}}
-            initialValues={{remember: true}}
-            autoComplete="off"
-            onFinish={createUser}
-        >
-            <Form.Item label="First name" name="firstName" required><Input/></Form.Item>
-            <Form.Item label="Last name" name="lastName"><Input/></Form.Item>
-            <Form.Item label="E-mail" name="email" required><Input/></Form.Item>
-            <Form.Item label="Password" name="password" required><Input.Password/></Form.Item>
-            <Form.Item label="Repeat password" name="repeatPassword" required><Input.Password/></Form.Item>
-            { error && <Text type="danger" strong="true">{ error }</Text> }
-            <Form.Item wrapperCol={{offset: 9, span: 5}}>
-                <Button type="primary" htmlType="submit">Submit</Button>
-            </Form.Item>
-        </Form>
+        <div class="form-body">
+          <div class="mainregisterform">
+            <div class="registerform">
+              <label class="registerlabel">Register</label>
+              <input class="registerinput" placeholder="First name" onChange={handleFirstNameChange} required />
+              <input class="registerinput" placeholder="Last name" onChange={handleLastNameChange} required />
+              <input class="registerinput" placeholder="E-mail" onChange={handleEmailChange} name="email" type="email" required />
+              <input class="registerinput" placeholder="Password" onChange={handlePasswordChange} type="password" required />
+              <input class="registerinput" placeholder="Repeat password" onChange={handleRepeatPasswordChange} type="password" required />
+              <button class="registerbutton" type="primary" onClick={createUser}>Submit</button>
+            </div>
+          </div>
+        </div>
     )
 }
-
-
 export default Register
